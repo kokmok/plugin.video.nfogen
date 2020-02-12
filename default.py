4cac973e76c3dff8690e6244f014e373f1a47a5e
kokmok <jona303@gmail.com>
2020-02-13 00:21:06 +0100

    use xbmc vfs

--- default.py
+++ default.py
@@ -150,7 +150,7 @@ def createTvShowNfo(showName, showNumber, picture, files, folder):
 	seasonXML += "\n</tvshow>"
 	xbmc.log(("writing nfo file " + folder + "tvshow.nfo").encode('utf8'), level=xbmc.LOGINFO)
 	text_file = xbmcvfs.File(folder + "tvshow" + ".nfo", "w+")
-	text_file.write(episodeXML)
+	text_file.write(seasonXML)
 	text_file.close()
 
 
